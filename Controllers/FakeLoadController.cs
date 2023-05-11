467498bbcda8107f1c8d65f203f96d2d090be128
tuchin1228 <jhinwei1228@gmail.com>
2023-05-11 16:34:26 +0800

    加入專案檔案。

---
+++ Controllers/FakeLoadController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ScaleTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakeLoadController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(int seconds, int percentage)
+        {
+            FakeWorkload(seconds, percentage);
+
+            return Ok($"Fake load of {percentage}% for {seconds} seconds");
+        }
+
+        // Method to simulate cpu load for a period of time        
+        private void FakeWorkload(int seconds, int percentageCpu)
+        {
+            percentageCpu = Math.Max(100, percentageCpu);
+            var end = DateTime.Now.AddSeconds(seconds);
+            var percentage = percentageCpu;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            while (DateTime.Now < end)
+            {
+                // Make the loop go on for "percentage" milliseconds then sleep the
+                // remaining percentage milliseconds. So 40% utilization means work 40ms and sleep 60ms
+                if (watch.ElapsedMilliseconds > percentage)
+                {
+                    System.Threading.Thread.Sleep(100 - percentage);
+                    watch.Reset();
+                    watch.Start();
+                }
+            }
+        }
+    }
+}
